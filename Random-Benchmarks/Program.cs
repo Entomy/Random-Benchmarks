a96cb6f88161ab7aa61eb659448c0e1f5ec9d2e7
Patrick Kelly <p-kell@live.com>
2019-10-26 21:33:17 -0400

    Implemented benchmark selector using Consolator

--- Random-Benchmarks/Program.cs
+++ Random-Benchmarks/Program.cs
@@ -1,11 +1,40 @@
-﻿using BenchmarkDotNet.Running;
+﻿using System;
+using BenchmarkDotNet.Running;
+using Consolator.UI;
+using Consolator.UI.Theming;
+using Console = Consolator.Console;
 
 namespace Random_Benchmarks {
 	public static class Program {
+		static Program() {
+			Theme.DefaultDark.Apply();
+		}
+
 		public static void Main() {
+			Console.WriteChoices(Choices);
+			Console.ReadChoice(Choices);
+		}
+
+		internal static KeyChoiceSet Choices = new KeyChoiceSet(" Enter Choice: ",
+				new KeyChoice(ConsoleKey.D1, nameof(DynamicDispatchOverhead), DynamicDispatchOverheadChoice),
+				new KeyChoice(ConsoleKey.D2, nameof(DispatchApproaches), DispatchApproachesChoice),
+				new KeyChoice(ConsoleKey.D3, nameof(VectorMath), VectorMathChoice),
+				new BackKeyChoice(ConsoleKey.Q, "Quit", () => Environment.Exit(0)));
+
+
+		internal static void DynamicDispatchOverheadChoice() {
 			BenchmarkRunner.Run<DynamicDispatchOverhead>();
+			Main();
+		}
+
+		internal static void DispatchApproachesChoice() {
 			BenchmarkRunner.Run<DispatchApproaches>();
+			Main();
+		}
+
+		internal static void VectorMathChoice() {
 			BenchmarkRunner.Run<VectorMath>();
+			Main();
 		}
 	}
 }
