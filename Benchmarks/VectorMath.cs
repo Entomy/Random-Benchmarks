5636bf983a92b1454d4ec8d29e32a12ba130b649
Patrick Kelly <p-kell@live.com>
2020-08-24 15:27:56 -0400

    Added more to vector math benchmarks

--- Benchmarks/VectorMath.cs
+++ Benchmarks/VectorMath.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Numerics;
+using System.Runtime.InteropServices;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Jobs;
 
@@ -11,15 +13,20 @@ namespace RandomBenchmarks {
 	public class VectorMath {
 		// This does UInt16 vectors just because I needed the specific numbers. It still shows off how much faster the vector unit is, regardless.
 
-		public UInt16[] Array = new UInt16[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+		public static UInt16[] Array = new UInt16[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
 
-		public Vector<UInt16> Vector;
-
-		[Benchmark]
-		public void CreateArray() => Array = new UInt16[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
-
-		[Benchmark]
-		public void CreateVector() => Vector = new Vector<UInt16>(Array);
+		public static String String = "Hello world";
+		public static Vector<UInt16> StrVector;
+		public static Vector<UInt16> Vector = new Vector<UInt16>(Array);
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Minor Code Smell", "S3963:\"static\" fields should be initialized inline", Justification = "<Pending>")]
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1810:Initialize reference type static fields inline", Justification = "<Pending>")]
+		static VectorMath() {
+			UInt16[] ints = new UInt16[16];
+			for (Int32 i = 0; i < String.Length; i++) {
+				ints[i] = unchecked((UInt16)String[i]);
+			}
+			StrVector = new Vector<UInt16>(ints);
+		}
 
 		[Benchmark]
 		public Int32[] AddArray() {
@@ -34,6 +41,17 @@ namespace RandomBenchmarks {
 		public Vector<UInt16> AddVector() => Vector + Vector;
 
 		[Benchmark]
+		public void CreateArray() => Array = new UInt16[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+
+		[Benchmark]
+		public void CreateVector() => Vector = new Vector<UInt16>(Array);
+
+		[Benchmark]
+		public void CreateVectorFromString() {
+			StrVector = new Vector<UInt16>(new Converter(String).Units);
+		}
+
+		[Benchmark]
 		public Boolean EqualsArray() {
 			Boolean Result = true;
 			for (UInt16 i = 0; i < Array.Length; i++) {
@@ -43,6 +61,28 @@ namespace RandomBenchmarks {
 		}
 
 		[Benchmark]
+		public Boolean EqualsSpan() => String.AsSpan().Equals("Hello world".AsSpan(), StringComparison.Ordinal);
+
+		[Benchmark]
 		public Boolean EqualsVector() => Vector == Vector;
+
+		[Benchmark]
+		public Boolean NotEqualsVector() => Vector != StrVector;
+
+		[StructLayout(LayoutKind.Explicit)]
+		private struct Converter {
+			[FieldOffset(0)] private Char[] chars;
+			[FieldOffset(0)] private UInt16[] units;
+
+			public Converter(String @string) {
+				units = null;
+				chars = new Char[16];
+				@string.AsSpan().CopyTo(chars.AsSpan(0, @string.Length));
+			}
+
+			public Char[] Chars => chars;
+
+			public UInt16[] Units => units;
+		}
 	}
 }
